ab04d07f7d6e60aac03b8192ad7b7a52c0980b59
caxzy#3907 <cxrdopvc@gmail.com>
2026-08-07 17:07:11 +0200

    Update "description".

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 
 export const metadata: Metadata = {
   title: 'VexyraHub - Your Hub for optimization.',
-  description: 'VexyraHub - A hub of PC optimization tools, programs, and guides for Windows 10/11. All in one place and 100% open source.',
+  description: 'A hub of PC optimization tools, programs, and guides for Windows 10/11. All in one place and 100% open source.',
 };
 
 export default function RootLayout({
