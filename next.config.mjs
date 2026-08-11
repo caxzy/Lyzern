f2b2e5649a59ec6cdffe72b94ac4f5124283c7ee
caxzy#3907 <108175829+caxzy@users.noreply.github.com>
2026-08-11 23:53:48 +0200

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@ import nextra from 'nextra'
  
 // Set up Nextra with its configuration
 const withNextra = nextra({
-    output: 'export',
+    theme: 'nextra-theme-docs',
+    themeConfig: '/theme.config.jsx'
 
 })
  
