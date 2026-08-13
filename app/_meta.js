6bedb82f56b2ecef984d634dd105b86e47a8991f
caxzy#3907 <108175829+caxzy@users.noreply.github.com>
2026-08-13 13:42:46 +0200

    Update _meta.js

--- app/_meta.js
+++ app/_meta.js
@@ -2,9 +2,11 @@ export default {
   index: {
     display: 'hidden',
   },
+  ///
     introduction: "Introduction to Vexyra.",
   "###": {
     type: "separator",
   },
+  ///
     win_optimization: "docs2 Test.",
 }
