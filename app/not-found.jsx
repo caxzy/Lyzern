8df1c98a403431cbe26cb4facaf30cb2d137de9e
caxzy <cxrdopvc@gmail.com>
2026-08-16 13:39:22 +0200

    Added "NotFound" page using nextra.site function.

---
+++ app/not-found.jsx
@@ -0,0 +1,14 @@
+export default function NotFound() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '80vh',
+      }}
+    >
+      <h1 style={{ fontSize: '2rem', fontWeight: 'bold' }}>Page not found.</h1>
+    </div>
+  )
+}
