69e6357f1e8f7c0889c5b988b19b1437220c801d
caxzy <cxrdopvc@gmail.com>
2026-08-18 22:57:03 +0200

    Added "Windows tools and optimizers", and added some fun emojis.
    Nuh Uh.

--- app/_meta.js
+++ app/_meta.js
@@ -3,14 +3,16 @@ export default {
     display: 'hidden',
   },
   ///
-    introduction: "Introduction to Vexyra.",
+    introduction: "👋 Introduction to Vexyra.",
   "###": {
     type: "separator",
   },
   ///
-    windows_optimization: "Getting Started with Windows Optimization. ",
+    tools_optimizers: "🛠️ Useful Windows tools and optimizers. ",
       "###": {
     type: "separator",
   },
+  //
+
   //
 }
---
+++ app/tools_optimizers/_meta.js
@@ -0,0 +1,5 @@
+export default {
+  optimizers: "List of Windows Optimizers",
+  win_cleaners: "List of Windows Cleaners",
+  win_tools: "List of Windows Tools"
+};
